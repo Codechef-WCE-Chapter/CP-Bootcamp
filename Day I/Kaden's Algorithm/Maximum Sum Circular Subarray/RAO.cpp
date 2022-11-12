f9395cbe72a097849e6842910ffaec5d6bcad869
RAO.exe <84059138+RAOexe@users.noreply.github.com>
2022-11-12 15:43:30 +0530

    Update RAO.cpp

--- Day I/Kaden's Algorithm/Maximum Sum Circular Subarray/RAO.cpp
+++ Day I/Kaden's Algorithm/Maximum Sum Circular Subarray/RAO.cpp
@@ -1,1 +1,27 @@
-int main(){}
+class Solution {
+public:
+int kAlgo(vector<int>& nums)
+{
+    int maxCurr=nums[0];
+    int maxAll=nums[0];
+    for(int i=1;i<nums.size();i++)
+    {
+        maxCurr=max(nums[i],maxCurr+nums[i]);
+        maxAll = max(maxAll,maxCurr);
+    }
+    return maxAll;
+}
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int max_normal =kAlgo(nums);
+        if(max_normal<0)
+        return max_normal;
+        int sum=0;
+        for(int i=0;i<nums.size();i++)
+            sum += nums[i];
+        int res1=kAlgo(nums);
+        for(int i=0;i<nums.size();i++)
+            nums[i]=-1*nums[i];
+        int res2 = kAlgo(nums);
+        return max(res1,(sum+res2));
+    }
+};
